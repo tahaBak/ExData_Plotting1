98a99977f1efe8933aed76323ba532d99f8cc96c
tahaBak <tahabakhouch@gmail.com>
2016-02-26 02:29:10 +0100

    plot1 done

---
+++ plot1.R
@@ -0,0 +1,27 @@
+
+getwd()
+
+
+myData<-read.csv("household_power_consumption.txt", sep=";",colClasses = c("character","character","numeric","numeric","numeric",
+                                                                           "numeric","numeric","numeric"),na.strings="?")
+
+myData$Date<-as.Date(myData$Date,format="%d/%m/%Y")
+head(myData$Date)
+
+startDate<-as.Date("01/02/2007",format="%d/%m/%Y")
+startDate
+endDate<-as.Date("2/2/2007",format="%d/%m/%Y")
+endDate
+
+
+usefulData<-myData[(myData$Date>= startDate)&(myData$Date<= endDate),]
+head(usefulData)
+tail(usefulData)
+
+class(usefulData$Global_active_power)
+
+hist(usefulData$Global_active_power,main="Global Active Power", col="red",xlab="Global Active Power (Kilowatts)")
+
+colnames(myData)
+head(myData)
+
