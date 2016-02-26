330099475c99e50aa4e55fd8073fa276037fc74f
tahaBak <tahabakhouch@gmail.com>
2016-02-26 03:26:26 +0100

    plot2 done

---
+++ plot2.R
@@ -0,0 +1,30 @@
+
+getwd()
+
+
+myData<-read.csv("household_power_consumption.txt", sep=";",colClasses = c("character","character","numeric","numeric","numeric",
+                                                                           "numeric","numeric","numeric"),na.strings="?")
+
+myData$Date<-as.Date(myData$Date,format="%d/%m/%Y")
+
+startDate<-as.Date("01/02/2007",format="%d/%m/%Y")
+startDate
+endDate<-as.Date("2/2/2007",format="%d/%m/%Y")
+endDate
+
+
+usefulData<-myData[(myData$Date>= startDate)&(myData$Date<= endDate),]
+head(usefulData)
+tail(usefulData)
+
+usefulData$Date<-as.character(usefulData$Date)
+
+usefulData$timeAndDate<-paste(usefulData$Date,usefulData$Time,sep=":")
+head(usefulData.timeAndDate)
+
+usefulData$timeAndDateFormat<-strptime(usefulData.timeAndDate,format="%Y-%m-%d:%T")
+head(usefulData$timeAndDateFormat)
+
+length(usefulData$timeAndDateFormat)
+length(usefulData$Global_active_power)
+plot(usefulData$timeAndDateFormat,usefulData$Global_active_power,type="l",xlab="",ylab="Global Active Power (kilowatts)")
